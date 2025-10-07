94841a431005efaebfa7a5e55141683bc3289d2d
Shajibul Hasan Soaib <shajibulhasan.puc@gmail.com>
2025-10-07 22:27:53 +0600

    class 1 & 2

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'home.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
---
+++ lib/home.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter-Batch 13", style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.green,
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Text(
+                    "Flutter Batch 13",
+                    style: TextStyle(fontSize: 30, color: Colors.blue),
+                  ),
+                  Text(
+                    "Flutter Batch 13",
+                    style: TextStyle(fontSize: 30, color: Colors.blue),
+                  ),
+                  Text(
+                    "Flutter Batch 13",
+                    style: TextStyle(fontSize: 30, color: Colors.blue),
+                  ),
+                  Text(
+                    "Flutter Batch 13",
+                    style: TextStyle(fontSize: 30, color: Colors.blue),
+                  ),
+                  Text(
+                    "Flutter Batch 13",
+                    style: TextStyle(fontSize: 30, color: Colors.blue),
+                  ),
+                  Text(
+                    "Flutter Batch 13",
+                    style: TextStyle(fontSize: 30, color: Colors.blue),
+                  ),
+                  Text(
+                    "Flutter Batch 13",
+                    style: TextStyle(fontSize: 30, color: Colors.blue),
+                  ),
+                ],
+              ),
+            ),
+            Text(
+              "Flutter Batch 13",
+              style: TextStyle(fontSize: 30, color: Colors.blue),
+            ),
+            Text(
+              "Flutter Batch 13",
+              style: TextStyle(fontSize: 30, color: Colors.blue),
+            ),
+            Text(
+              "Flutter Batch 13",
+              style: TextStyle(fontSize: 30, color: Colors.blue),
+            ),
+            Text(
+              "Flutter Batch 13",
+              style: TextStyle(fontSize: 30, color: Colors.blue),
+            ),
+            Text(
+              "Flutter Batch 13",
+              style: TextStyle(fontSize: 30, color: Colors.blue),
+            ),
+            Text(
+              "Flutter Batch 13",
+              style: TextStyle(fontSize: 30, color: Colors.blue),
+            ),
+            Text(
+              "Flutter Batch 13",
+              style: TextStyle(fontSize: 30, color: Colors.blue),
+            ),
+            Text(
+              "Flutter Batch 13",
+              style: TextStyle(fontSize: 30, color: Colors.blue),
+            ),
+            Text(
+              "Flutter Batch 13",
+              style: TextStyle(fontSize: 30, color: Colors.blue),
+            ),
+            Text(
+              "Flutter Batch 13",
+              style: TextStyle(fontSize: 30, color: Colors.blue),
+            ),
+            Text(
+              "Flutter Batch 13",
+              style: TextStyle(fontSize: 30, color: Colors.blue),
+            ),
+            Text(
+              "Flutter Batch 13",
+              style: TextStyle(fontSize: 30, color: Colors.blue),
+            ),
+            Text(
+              "Flutter Batch 13",
+              style: TextStyle(fontSize: 30, color: Colors.blue),
+            ),
+            Text(
+              "Flutter Batch 13",
+              style: TextStyle(fontSize: 30, color: Colors.blue),
+            ),
+            Text(
+              "Flutter Batch 13",
+              style: TextStyle(fontSize: 30, color: Colors.blue),
+            ),
+            Text(
+              "Flutter Batch 13",
+              style: TextStyle(fontSize: 30, color: Colors.blue),
+            ),
+            Text(
+              "Flutter Batch 13",
+              style: TextStyle(fontSize: 30, color: Colors.blue),
+            ),
+            Text(
+              "Flutter Batch 13",
+              style: TextStyle(fontSize: 30, color: Colors.blue),
+            ),
+            Text(
+              "Flutter Batch 13",
+              style: TextStyle(fontSize: 30, color: Colors.blue),
+            ),
+            Text(
+              "Flutter Batch 13",
+              style: TextStyle(fontSize: 30, color: Colors.blue),
+            ),
+            Text(
+              "Flutter Batch 13",
+              style: TextStyle(fontSize: 30, color: Colors.blue),
+            ),
+            Text(
+              "Flutter Batch 13",
+              style: TextStyle(fontSize: 30, color: Colors.blue),
+            ),
+            Text(
+              "Flutter Batch 13",
+              style: TextStyle(fontSize: 30, color: Colors.blue),
+            ),
+            Text(
+              "Flutter Batch 13",
+              style: TextStyle(fontSize: 30, color: Colors.blue),
+            ),
+            Text(
+              "Flutter Batch 13",
+              style: TextStyle(fontSize: 30, color: Colors.blue),
+            ),
+            Text(
+              "Flutter Batch 13",
+              style: TextStyle(fontSize: 30, color: Colors.blue),
+            ),
+            Text(
+              "Flutter Batch 13",
+              style: TextStyle(fontSize: 30, color: Colors.blue),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
